a280cbde88563539aa0dde418118cd9234dc5256
Deepthi Gangadhar <deepthitg995@gmail.com>
2026-03-13 07:35:21 +0100

    Fix merge conflicts

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 "use client";
 
->>>>>>> 80365d441e2c6bed578a07aa31c847d067e6b3bd
 import { Suspense } from "react";
 import ReportContent from "./ReportContent";
 
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,8 +4,4 @@ const config = {
   },
 };
 
-<<<<<<< HEAD
 export default config;
-=======
-export default config;
->>>>>>> 80365d441e2c6bed578a07aa31c847d067e6b3bd
